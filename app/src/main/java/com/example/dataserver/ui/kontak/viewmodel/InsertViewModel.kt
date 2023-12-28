f8854eb89ad7fbf4b17c17aaa5738d0d1658da2a
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2023-12-28 19:37:14 +0700

    add isi class InsertViewModel

--- app/src/main/java/com/example/dataserver/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/dataserver/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.dataserver.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.dataserver.repository.InsertUiEvent
+import com.example.dataserver.repository.InsertUiState
 import com.example.dataserver.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 }
