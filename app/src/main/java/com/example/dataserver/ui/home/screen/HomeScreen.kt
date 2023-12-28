7cd737aaebe2f156788952b09d3f856d3b2339b6
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2023-12-28 11:55:34 +0700

    tambah perandaian detail click di fun homestatus

--- app/src/main/java/com/example/dataserver/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/dataserver/ui/home/screen/HomeScreen.kt
@@ -51,7 +51,11 @@ fun HomeStatus(
 ){
     when (kontakUIState){
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
-        is KontakUIState.Success -> KontakLayout(kontak = kontakUIState.kontak, modifier = modifier.fillMaxWidth()
+        is KontakUIState.Success -> KontakLayout(
+            kontak = kontakUIState.kontak, modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id)
+            }
         )
 
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
