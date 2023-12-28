350e7a3153eb906d68812c948adb9c2475002b8c
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2023-12-28 19:34:16 +0700

    create class InsertViewModel

---
+++ app/src/main/java/com/example/dataserver/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.dataserver.ui.kontak.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.dataserver.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+}
