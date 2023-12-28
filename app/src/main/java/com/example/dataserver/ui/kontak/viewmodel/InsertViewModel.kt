df112427a862cb251a41918f127c22ed1b77594c
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2023-12-28 19:41:58 +0700

    add data class InsertUiEvent

--- app/src/main/java/com/example/dataserver/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/dataserver/ui/kontak/viewmodel/InsertViewModel.kt
@@ -33,3 +33,10 @@ class InsertViewModel(private val kontakRepository: KontakRepository) : ViewMode
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
+
+data class InsertUiEvent(
+    val id : Int = 0,
+    val nama : String = "",
+    val email : String = "",
+    val noHp : String = "",
+)
